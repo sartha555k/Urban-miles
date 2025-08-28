957805dd944d43e803e117178c5f6ed9c6bec7e1
Sarthak Patel <sarthak.code30@gmail.com>
2025-08-28 13:02:13 +0530

    fix: correct header property name in authCaptain middleware

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -31,7 +31,7 @@ module.exports.authUser = async (req, res, next) => {
 };
 
 module.exports.authCaptain = async (req, res, next) => {
-  const token = req.cookies.token || req.header.authorization?.split(" ")[1];
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if(!token){
     return res.status(401).json({ message: "Unauthorized !!" });
   }
